bc77f56d78f0f7e484c81e79e5e52ef57ab78221
ArnoDeVos <arno.de.vos@student.ehb.be>
2021-12-06 21:05:25 +0100

    adding challenges

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 "use strict"
 
+const url = "https://web2-groupproject-backend.herokuapp.com"
 window.onload = function () {
     Challenges();
 
     document.getElementById('form').addEventListener('submit', e => {
         e.preventDefault();
+        saveChallenges();
     })
 }
 
 function Challenges() {
-    fetch('https://web2-groupproject-backend.herokuapp.com/challenges')
+    fetch(url+"/challenges")
         .then(response => response.json())
         .then(function (data) {
             console.log(data);
@@ -17,12 +19,14 @@ function Challenges() {
 
             data.forEach(element => {
                 htmlString += `
-                <div class="card">
+                <div class="card text-center">
                 <h5 class="card-title">${element.name}</h5>
                 <div class="card-body">
-                <p class="card-text">${element.points}</p>
-                <p class="card-text">${element.course}</p>
-                <p class="card-text">${element.session}</p>
+                <p class="card-text">${element.points} points</p>
+                <p class="card-text">course: ${element.course}</p>
+                <p class="card-text">session: ${element.session}</p>
+                <button id="delete" type="submit" class="btn btn-danger"> Delete</button>
+                <button id="edit" type="submit" class="btn btn-success"> Edit</button>
                 </div>
                 </div>`;
                 
@@ -32,3 +36,23 @@ function Challenges() {
             document.getElementById("listblock").insertAdjacentHTML('beforeend', htmlString)
         });
     }
+
+    async function saveChallenges(){
+
+            const data = JSON.stringify({
+        "name": document.getElementById('name').value,
+        "points": document.getElementById('points').value,
+        "course": document.getElementById('course').value,
+        "session": document.getElementById('session').value
+        })
+            fetch(url+"/challenge", {
+                    method: 'POST',
+                    body: data
+                })
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data.json());
+                });
+        await Challenges();
+
+            }
